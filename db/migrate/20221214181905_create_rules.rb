6541f02e02367081680ca2afe8e075374a65e7c5
LeraRad <exlab7@gmail.com>
2022-12-14 19:53:34 +0100

    Created rules entity

---
+++ db/migrate/20221214181905_create_rules.rb
@@ -0,0 +1,11 @@
+class CreateRules < ActiveRecord::Migration[7.0]
+  def change
+    create_table :rules do |t|
+      t.string :title
+      t.text :body
+      t.text :image
+
+      t.timestamps
+    end
+  end
+end
