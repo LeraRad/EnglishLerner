66852376f0812baf4f82af43816421f0f85dc6ea
LeraRad <exlab7@gmail.com>
2022-12-14 20:16:48 +0100

    Some actions to Word entity was added. Still need to correct it bcos of problems with id param

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,2 +1,57 @@
 class WordsController < ApplicationController
+  def index
+    @word = Word.all
+  end
+
+  def new
+    @word = Word.new
+  end
+
+  def create
+      @word = Word.new(params.require(:word).permit(:title,:subtitle,:body))
+
+      respond_to do |format|
+        if @word.save
+          format.html { redirect_to words_path, notice: "New Word was successfully created." }
+          format.json { render :show, status: :created, location: @word }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @word.errors, status: :unprocessable_entity }
+        end
+      end
+  end
+
+  def edit
+    @word = Word.find(params[:id])
+  end
+
+  def update
+      @word = Word.find(params[:id])
+      respond_to do |format|
+        if @word.update(params.require(:word).permit(:title,:subtitle,:body))
+          format.html { redirect_to words_path, notice: "The record was successfully updated." }
+          format.json { render :show, status: :ok, location: @word }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @word.errors, status: :unprocessable_entity }
+        end
+      end
+  end
+
+  def show
+    @word = Word.find(params[:id])
+  end
+
+  def destroy
+    # Perform the lookup
+    @word = Word.find(params[:id])
+
+    #Destroy/delete the record
+    @word.destroy
+
+    #Redirect
+    respond_to do |format|
+      format.html { redirect_to words_url, notice: "Word was removed." }
+    end
+  end
 end
