6c20b6cb356eada25e2a9301cac6997790bd8167
LeraRad <exlab7@gmail.com>
2023-01-09 10:54:10 +0100

    Update, Edit, Index & Create functions are working. Next step: some links and show feature

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -8,16 +8,34 @@ class WordsController < ApplicationController
   end
 
   def create
-      @word = Word.new(params.require(:eng_word).permit(:eng_word,:transcription,:translation))
+    @word = Word.new(params.require(:word).permit(:eng_word,:transcription,:translation))
 
+    respond_to do |format|
+      if @word.save
+        format.html { redirect_to words_path, notice: "New Word was successfully added." }
+        format.json { render :show, status: :created, location: @word }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @word.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+    @word = Word.find(params[:id])
+  end 
+
+def update
+      @word = Word.find(params[:id])
       respond_to do |format|
-        if @word.save
-          format.html { redirect_to words_path, notice: "New Word was successfully added." }
-          format.json { render :show, status: :created, location: @word }
+        if @word.update(params.require(:word).permit(:eng_word,:translation,:transcription))
+          format.html { redirect_to words_path, notice: "The word was successfully updated." }
+          format.json { render :show, status: :ok, location: @word }
         else
-          format.html { render :new, status: :unprocessable_entity }
+          format.html { render :edit, status: :unprocessable_entity }
           format.json { render json: @word.errors, status: :unprocessable_entity }
         end
       end
   end
+
 end
